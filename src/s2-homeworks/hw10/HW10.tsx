f6f5a640ae2e9bf8b5639755f9cb018c371fbeba
Ilya <shulapov1999@gmail.com>
2023-04-07 15:27:19 +0300

    additional fix

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -12,12 +12,12 @@ import {loadingAC} from "./bll/loadingReducer";
 const HW10 = () => {
 
     const dispatch = useDispatch()
-    const isLoading = useSelector<AppStoreType, {isLoading: boolean}>(state => state.loading)
-    console.log(isLoading.isLoading)
+    const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
+    console.log(isLoading)
     const setLoading = () => {
-        dispatch(loadingAC(isLoading.isLoading))
+        dispatch(loadingAC(isLoading))
         setTimeout(() => {
-            dispatch(loadingAC(!isLoading.isLoading))
+            dispatch(loadingAC(!isLoading))
         }, 1500)
     }
 
@@ -27,7 +27,7 @@ const HW10 = () => {
             <div className={s2.hwTitle}>Homework #10</div>
             <hr className={s2.hr10_1}/>
             <div className={s.hw}>
-                {isLoading.isLoading ? (
+                {isLoading ? (
                     <div id={'hw10-loading'}>
                         <Loader/>
                     </div>
