95300a4cb997a84b403a9e24b70cfc00994f56d3
Ilya <shulapov1999@gmail.com>
2023-04-09 23:01:19 +0300

    HW11 finish logic

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -16,8 +16,12 @@ function HW11() {
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
     const change = (event: Event, value: number | number[]) => {
-        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-        console.log(value)
+
+        if(Array.isArray(value)) {
+            setValue1(value[0])
+            setValue2(value[1])
+        } else setValue1(value)
+
     }
 
     return (
@@ -30,7 +34,9 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
+                            value={value1}
                             onChange={change}
+                            // сделать так чтоб value1 изменялось // пишет студент
 
                         />
                     </div>
@@ -38,7 +44,8 @@ function HW11() {
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
-                            onChange={() => {}}
+                            value={[value1, value2]}
+                            onChange={change}
                             // сделать так чтоб value1/2 изменялось // пишет студент
 
                         />
