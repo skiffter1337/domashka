243fe96393024e802286a1d0f20fe27efa22fc5a
Ilya <shulapov1999@gmail.com>
2023-04-23 14:04:34 +0300

    HW13 finish logic

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -71,7 +71,7 @@ const HW13 = () => {
 
     return (
         <div id={'hw13'}>
-            <div className={s2.hwTitle}>Homework #13 -</div>
+            <div className={s2.hwTitle}>Homework #13 </div>
 
             <div className={s2.hw}>
                 <div className={s.buttonsContainer}>
